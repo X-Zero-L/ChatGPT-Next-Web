1204afd4ee80e6e8bc7e4f88c5666105ac441e31
Zero <98764734+X-Zero-L@users.noreply.github.com>
2024-06-28 16:51:29 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -139,6 +139,12 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 const openaiModels = [
   "gpt-3.5-turbo-0125",
   "qwen-max",
+  "qwen-turbo",
+  "qwen-plus",
+  "gpt-3.5-turbo-0125-internet",
+  "qwen-max-internet",
+  "qwen-turbo-internet",
+  "qwen-plus-internet",
   "gpt-4o-2024-05-13",
   "gpt-4-turbo-2024-04-09",
 ];
