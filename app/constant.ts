131ebb1f19b735fe92cec6c4a87c94cf7c7b9926
Zero <98764734+X-Zero-L@users.noreply.github.com>
2024-08-09 13:03:52 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -254,7 +254,6 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 };
 
 const openaiModels = [
-  "gpt-3.5-turbo-0125",
   "gpt-4o-mini-2024-07-18",
   "gpt-4-turbo-2024-04-09",
   "gpt-4o-2024-08-06",
